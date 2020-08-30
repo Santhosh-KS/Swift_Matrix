259bed824279dee52e7b78819a3ceb989ff33bbb
Santhosh K S <santhoshachar08@gmail.com>
2020-08-30 21:55:32 +0200

    Enabling all tests

--- Tests/MatrixTests/XCTestManifests.swift
+++ Tests/MatrixTests/XCTestManifests.swift
@@ -1,12 +1,12 @@
 #if !canImport(ObjectiveC)
-import XCTest
+    import XCTest
 
-public func __allTests() -> [XCTestCaseEntry] {
-    return [
-        //testCase(MatrixIntAndUintInitTests.__allTests__MatrixInitTests),
-        //testCase(MatrixFloatAndDoubleInitTests.__allTests__MatrixInitTests),
-        //testCase(MatrixIndexingOperations.__allTests__MatrixIndexTests),
-        testCase(MatrixOperationsTests.__allTests__MatrixOperationsTests),
-    ]
-}
+    public func __allTests() -> [XCTestCaseEntry] {
+        return [
+            testCase(MatrixIntAndUintInitTests.__allTests__MatrixInitTests),
+            testCase(MatrixFloatAndDoubleInitTests.__allTests__MatrixInitTests),
+            testCase(MatrixIndexingOperations.__allTests__MatrixIndexTests),
+            testCase(MatrixOperationsTests.__allTests__MatrixOperationsTests),
+        ]
+    }
 #endif
