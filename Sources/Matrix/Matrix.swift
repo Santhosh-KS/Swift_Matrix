7c088fb16ddc16436e6a6b6e7cc0bab710e15255
Santhosh K S <santhoshachar08@gmail.com>
2020-09-01 18:52:30 +0200

    cleanup based on the fourm discusion: https://forums.swift.org/t/generic-specialization-in-init-functions/39926

--- Sources/Matrix/Matrix.swift
+++ Sources/Matrix/Matrix.swift
@@ -2,21 +2,21 @@ import Foundation
 
 protocol MatrixContainer {
     associatedtype Element
-    init(_ rows:UInt, _ columns:UInt, with defaultVal:Element)
+    init(rows row:Int, columns column:Int, with defaultVal:Element)
     subscript(rowId:Int) -> [Element] { get }
     subscript(rowId:Int, colId:Int) -> Element { get }
 }
 
 public struct Matrix<Element: Numeric & Comparable > : MatrixContainer {
     var mat: Array<Array<Element>>
-    var rows: UInt
-    var columns: UInt
-    var size: UInt { return rows*columns}
-    var shape: (rows: UInt, columns: UInt) { return (rows, columns) }
+    var rows: Int
+    var columns: Int
+    var size: Int { return rows*columns}
+    var shape: (rows: Int, columns: Int) { return (rows, columns) }
 
-    public init(_ rows:UInt, _ columns:UInt, with defaultVal:Element) {
-        self.rows = rows
-        self.columns = columns
+    public init(rows r:Int, columns c:Int, with defaultVal:Element) {
+        self.rows = r
+        self.columns = c
         self.mat = Array.init(repeating: Array.init(repeating: defaultVal, count: Int(columns)), count: Int(rows))
     }
 
@@ -32,38 +32,22 @@ public struct Matrix<Element: Numeric & Comparable > : MatrixContainer {
     public subscript(idRow: Int, idCol:Int) -> Element {
         let interestedRow: [Element] = self[idRow]
         if idCol < 0 || idCol >= self.columns {
-           return interestedRow[Int(self.columns) - 1]
+           return interestedRow[self.columns - 1]
         } else {
            return interestedRow[idCol]
         }
     }
 }
 
-extension Matrix where Element == Double {
-    public init(_ rows:UInt, _ columns:UInt) {
-        self.init(rows, columns, with: 0.0)
-    }
-}
-
-extension Matrix where Element == Float {
-    public init(_ rows:UInt, _ columns:UInt) {
-        self.init(rows, columns, with: 0.0)
-    }
-}
-
-extension Matrix where Element == Int {
-    public init(_ rows:UInt, _ columns:UInt) {
-        self.init(rows, columns, with: 0)
-    }
-}
-
-extension Matrix where Element == UInt {
-    public init(_ rows:UInt, _ columns:UInt) {
-        self.init(rows, columns, with: 0)
-    }
-}
-
 extension Matrix {
+    public init(_ r:Int, _ c:Int) {
+        self.init(rows: r, columns: c, with: .zero)
+    }
+
+    public init(_ r:Int, _ c:Int, with defaultVal:Element) {
+        self.init(rows: r, columns: c, with: defaultVal)
+    }
+
     public init(_ arryOfArray:[[Element]]) {
         guard arryOfArray.count > 0 else {
             self.rows = 0
@@ -92,11 +76,11 @@ extension Matrix {
             }
         }
         self.mat = arryOfArray
-        self.rows = UInt(arryOfArray.count)
-        self.columns = UInt(colCount)
+        self.rows = arryOfArray.count
+        self.columns = colCount
     }
 
-    public init(_ array:[Element], _ row: UInt, _ col: UInt) {
+    public init(_ array:[Element], _ row: Int, _ col: Int) {
         guard array.count == row * col else {
             print("Elements count mismatch \(array.count) != \(row) * \(col)")
             self.rows = 0
@@ -110,19 +94,19 @@ extension Matrix {
         self.mat = array.reduceTo2d(row, col)
     }
 
-    public init(_ array:[Element], withRows rows:UInt) {
-        let columns: UInt = UInt(array.count) % rows
+    public init(_ array:[Element], withRows rows:Int) {
+        let columns: Int = array.count % rows
         self.init(array, rows, columns)
     }
 
-    public init(_ array:[Element], withColumns columns:UInt) {
-        let rows: UInt = UInt(array.count) % columns
+    public init(_ array:[Element], withColumns columns:Int) {
+        let rows: Int = array.count % columns
         self.init(array, rows, columns)
     }
 }
 
 extension Array  where Element : Numeric & Comparable {
-    internal func reduceTo2d(_ row: UInt, _ col: UInt) -> [[Element]] {
+    internal func reduceTo2d(_ row: Int, _ col: Int) -> [[Element]] {
         guard self.count == row*col else {
             print("Elements count does not match \(self.count) != \(row*col)")
             return [[Element]]()
@@ -131,7 +115,7 @@ extension Array  where Element : Numeric & Comparable {
         var tmpArray = [Element]()
         for (index, item) in self.enumerated()  {
             tmpArray.append(item)
-            if ((index+1) % Int(col) == 0) {
+            if ((index+1) % col == 0) {
                 retArray.append(tmpArray)
                 tmpArray.removeAll()
             }
@@ -139,13 +123,13 @@ extension Array  where Element : Numeric & Comparable {
         return retArray
     }
 
-    internal func reduceTo2d(withRows row:UInt) -> [[Element]] {
-        let col:UInt = UInt(self.count)/row
+    internal func reduceTo2d(withRows row:Int) -> [[Element]] {
+        let col:Int = self.count/row
         return reduceTo2d(row, col)
     }
 
-    internal func reduceTo2d(withColumns col:UInt) -> [[Element]] {
-        let row:UInt = UInt(self.count)/col
+    internal func reduceTo2d(withColumns col:Int) -> [[Element]] {
+        let row:Int = self.count/col
         return reduceTo2d(row, col)
     }
 }
